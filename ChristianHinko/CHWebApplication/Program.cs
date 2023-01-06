a2867af946222358cd2d6d471632ffc415078251
Christian Hinkle <c4hinkle@gmail.com>
2023-01-05 23:26:32 -0500

    Add MudBlazor package and add required setup.
    Add required stylesheets, scripts, services, and components.
    
    Delete css stylesheets because we will use MudBlazor's theming instead.

--- ChristianHinko/CHWebApplication/Program.cs
+++ ChristianHinko/CHWebApplication/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
