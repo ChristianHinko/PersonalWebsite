e2d64da174726d36c4767861b78610a68bbc1653
Christian Hinkle <c4hinkle@gmail.com>
2023-01-07 14:59:01 -0500

    Make CHThemeService and use it - with dark mode :D.
    Add the CHThemeService as a scoped service.
    
    Use the CHThemeService in the MainLayout's MudThemeProvider.
    
    Use the CHThemeService for toggling dark mode in the CHAppBar.

---
+++ ChristianHinko/CHWebApplication/Services/CHThemeService.cs
@@ -0,0 +1,47 @@
+﻿using MudBlazor;
+
+namespace CHWebApplication.Services
+{
+    /// <summary>
+    /// Central theming control over the web application.
+    /// </summary>
+    public class CHThemeService
+    {
+        /// <summary>
+        /// Current theme of the application.
+        /// </summary>
+        public MudTheme Theme { get; private set; }
+
+        /// <summary>
+        /// Determines whether to use the dark palette.
+        /// </summary>
+        public bool IsDarkMode { get; private set; }
+
+        /// <summary>
+        /// Invoked when theme changes occur. Use this for
+        /// notifying ComponentBase.StateHasChanged() when necessary.
+        /// </summary>
+        public event EventHandler? ThemeChanged;
+
+        public CHThemeService()
+        {
+            Theme = new();
+            IsDarkMode = false;
+            ThemeChanged = null;
+        }
+
+        public void SetIsDarkMode(bool isDarkMode)
+        {
+            IsDarkMode = isDarkMode;
+            if (ThemeChanged is not null)
+            {
+                ThemeChanged.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void ToggleIsDarkMode()
+        {
+            SetIsDarkMode(!IsDarkMode);
+        }
+    }
+}
