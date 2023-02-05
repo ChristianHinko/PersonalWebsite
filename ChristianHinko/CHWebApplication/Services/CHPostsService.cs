3dc8d6047000837d0c0d56453fc163260f3321f1
Christian Hinkle <c4hinkle@gmail.com>
2023-02-05 02:34:52 -0500

    Delete all test posts. Add horizontal dividers to Posts page.

--- ChristianHinko/CHWebApplication/Services/CHPostsService.cs
+++ ChristianHinko/CHWebApplication/Services/CHPostsService.cs
@@ -13,33 +13,6 @@ namespace CHWebApplication.Services
         {
             Posts = new List<CHPost>()
             {
-                new CHPost()
-                {
-                    Title = "Cool Post",
-                    Subtitle = "This is my really cool post where I talk about perspective and orthographic and the different projections of our three-dimensional world.",
-                    Date = new DateTime(2000, 01, 01),
-                    ImageSrc = "https://github.blog/wp-content/uploads/2021/06/GitHub-Bug-Bounty_for-social.png",
-                    Slug = "cool-post",
-                    Component = typeof(Shared.Components.Posts.CoolPost)
-                },
-                new CHPost()
-                {
-                    Title = "New Post Hyea",
-                    Subtitle = "Heyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfdaHeyh yeh hgasdljadohfda",
-                    Date = new DateTime(2023, 02, 01),
-                    ImageSrc = "https://thumbs.dreamstime.com/b/yeah-comic-vector-cartoon-illustration-explosions-comics-boom-symbol-sticker-tag-special-offer-label-advertising-badge-sign-banner-135602070.jpg",
-                    Slug = "new-post-hyea",
-                    Component = typeof(Shared.Components.Posts.CoolPost)
-                },
-                new CHPost()
-                {
-                    Title = "Evan With Rasin Bran On Top",
-                    Subtitle = "Even shows us how to make cerial with rason brand and krave.",
-                    Date = new DateTime(2023, 02, 01),
-                    ImageSrc = "https://www.kelloggs.com/content/dam/NorthAmerica/kelloggs/en_US/images/BrandDetail/Raisin-Bran/Raisin-Bran_1880x835_hero-image.jpg",
-                    Slug = "evan-with-rasin-brand-on-top-pog",
-                    Component = typeof(Shared.Components.Posts.EvanWithRasinBranOnTop)
-                }
             };
 
             // Sort Posts by date
