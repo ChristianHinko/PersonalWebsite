a421bba305bb0ec7c160b6da275ed265c458e254
Christian Hinkle <c4hinkle@gmail.com>
2023-02-07 00:26:01 -0500

    Make TrigFunctionsMakeSoMuchSense post.

--- ChristianHinko/CHWebApplication/Services/CHPostsService.cs
+++ ChristianHinko/CHWebApplication/Services/CHPostsService.cs
@@ -13,6 +13,15 @@ namespace CHWebApplication.Services
         {
             Posts = new List<CHPost>()
             {
+                new CHPost()
+                {
+                    Title = "Trig Functions Make So Much Sense!",
+                    Subtitle = "Visualize and understand the circular functions.",
+                    Date = new DateTime(2023, 02, 04),
+                    ImageSrc = "/images/TrigFunctionsMakeSoMuchSense.png",
+                    Slug = "trig-functions-make-so-much-sense",
+                    Component = typeof(Shared.Components.Posts.TrigFunctionsMakeSoMuchSense)
+                }
             };
 
             // Sort Posts by date
